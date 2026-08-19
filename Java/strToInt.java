2980e5dbfbedb2320ff65a63c8e89a74938feec4
unknown <gundojichandravamshi@gmail.com>
2026-08-19 13:16:40 +0530

    java str to int and num concat

---
+++ Java/strToInt.java
@@ -0,0 +1,8 @@
+public class strToInt {
+    public static void main(String[] args) {
+        String num = "5";
+        int x = Integer.parseInt(num);
+        System.out.println(x*2);
+    }
+    
+}
