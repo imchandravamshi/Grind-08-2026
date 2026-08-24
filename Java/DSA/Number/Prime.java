41b23f98f04d4bda04422e7f31d20880062d9784
unknown <gundojichandravamshi@gmail.com>
2026-08-24 23:14:23 +0530

    working with java

---
+++ Java/DSA/Number/Prime.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Prime {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if(num <= 1){
+            System.out.println("Not a prime number");
+        }
+    }
+}
+
